82fc781ad063af3666b9254d07ec625e7d7ef922
BRIGHT <wissen30@hotmail.com>
2023-12-23 13:47:48 +0300

    Add project files.

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialView.Models;
+using System.Diagnostics;
+
+namespace PartialView.Controllers
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+
+            return View(new TestModel()
+            {
+                Name = "Sadık",
+                Surname = "Turan"
+
+
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            //bu şekilde partial view açabiliyoruz.
+            return PartialView("_privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
